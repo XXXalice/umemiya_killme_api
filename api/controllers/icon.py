0891fe44313876cd2cb0024df16b2eb2c53f306b
梅宮 <euthanasia045@gmail.com>
2020-05-31 02:24:13 +0900

    add controller

--- api/controllers/icon.py
+++ api/controllers/icon.py
@@ -9,5 +9,15 @@ API_ROOT_PATH = os.path.join(KILLME_ROOT_PATH, VERSION)
 killme_path = Blueprint("killme_path", __name__)
 
 @killme_path.route(os.path.join(API_ROOT_PATH), methods=['GET'])
-def get_icon():
-    pass
+def index_page():
+
+    resp_body = jsonify({
+        "usage": [
+            {
+                "step1.": "This root path ({root}) \n"\
+                          "will be numbered with the icon you want.\n"\
+                          "(e.g.) {eg}".format(root = API_ROOT_PATH, eg = os.path.join(API_ROOT_PATH, str(10))),
+                "step2.": "If you don't know the number, you can specify 0 to enter random mode."
+            }
+        ]
+    })
