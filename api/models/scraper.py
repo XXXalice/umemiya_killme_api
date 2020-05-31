c300f7b121ec9165f78d3d1d1c1394bff4c56707
梅宮 <euthanasia045@gmail.com>
2020-05-31 20:41:02 +0900

    htmlの保存メソッドの定義

--- api/models/scraper.py
+++ api/models/scraper.py
@@ -31,12 +31,17 @@ class Crawler:
         return resp.text
 
     def _save_html(self, html_source):
+        import datetime
         """
         htmlの保存を内部で行う
         基本静的なページなので実行の度にリクエストを叩く必要はないため、頻繁なAPIの利用においてはこれで保存したhtmlからスクレイピングする
         :param html_source: アイコンページのhtmlだよ
         :return: ないよ
         """
+        file_name = datetime.datetime.now().strftime("%Y%m%d") + "_iconpage.html"
+        with open(file_name, "w") as f:
+            f.write(html_source)
+
 """
 スクレイピング機構
 """
